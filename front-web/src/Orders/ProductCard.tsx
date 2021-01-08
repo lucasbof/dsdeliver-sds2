aedf8c15875613f7e9a4a40e3e48c584942bed8a
Lucas Figueiredo Bof <lucasfbof@gmail.com>
2021-01-08 09:43:41 -0300

    Search order location

---
+++ front-web/src/Orders/ProductCard.tsx
@@ -0,0 +1,33 @@
+import { Product } from './types';
+
+type Props = {
+    product: Product;
+}
+
+const formatPrice = (price: number) => {
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+    return formatter.format(price);
+}
+
+const ProductCard = ({ product }: Props) => (
+    <div className="order-card-container">
+        <h3 className="order-card-title">
+            {product.name}
+        </h3>
+        <img src={product.imageUri} alt={product.name} className="order-card-image" />
+        <h3 className="order-card-price">
+            {formatPrice(product.price)}
+        </h3>
+        <div className="order-card-description">
+            <h3>Descrição</h3>
+            <p>
+                {product.description}
+            </p>
+        </div>
+    </div>
+);
+
+export default ProductCard;
---
+++ front-web/src/Orders/api.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:8080';
+
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
+
+export const fetchProducts = () => axios(`${API_URL}/products`);
+
+export const fetchLocalMapBox = (local: string) => 
+    axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
